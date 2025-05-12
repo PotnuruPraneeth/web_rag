5dda8e3c906fe8e1a252beb819fe78f627be2cc3
praneeth <potnurupraneeth48@gmail.com>
2025-05-12 16:51:58 +0530

    add

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -1,11 +1,8 @@
 import streamlit as st
-import langchain_community
 from langchain_community.vectorstores import Chroma
 from langchain_google_vertexai import VertexAIEmbeddings
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
-
-import streamlit as st
 
 # Set page config
 st.set_page_config(page_title="Robot Center", layout="wide")
